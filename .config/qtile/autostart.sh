f2c9bf1f83ad512a41b04efab35fa545ddb33c20
Niklas Herz <niklasherz@gmx.net>
2021-03-03 12:04:47 +0100

    xrandr and xinput in autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,8 +1,10 @@
-#! /bin/bash 
+#! /bin/bash
 lxsession &
 picom --experimental-backends &
 nitrogen --restore &
-urxvtd -q -o -f &
+#urxvtd -q -o -f &
 /usr/bin/emacs --daemon &
 volumeicon &
 nm-applet &
+xrandr --output DisplayPort-2 --mode 1920x1080 --rate 143.98 &
+xinput set-prop "pointer:Razer Razer DeathAdder V2 Pro" "libinput Accel Speed" -0.8 &
