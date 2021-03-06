4dde5965dedfff811a2504b2e9f17c2f0bd36d8f
Niklas Herz <niklasherz@gmx.net>
2021-03-06 19:24:36 +0100

    added projectile path

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -52,3 +52,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;;Projectile Settings
+(setq
+ projectile-project-search-path '("~/fh/")
+ )
