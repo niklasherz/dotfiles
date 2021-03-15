a9d6e38d3cf797975ce11c763e3ef42089b053f5
Niklas Herz <niklasherz@gmx.net>
2021-03-15 17:25:19 +0100

    display power management

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -12,6 +12,7 @@ xrandr --output DP-2 --mode 1920x1080 --rate 143.98 &
 xrandr --output DP-3 --mode 1920x1080 --rate 143.98 &
 #TODO Sensitivity Matrix and no Acceleration
 xinput set-prop "pointer:Razer Razer DeathAdder V2 Pro" "libinput Accel Speed" -0.8 &
+xset -dpms &
 polybar left &
 polybar middle &
 polybar right &
